e7010a9f4179a10208aa48f3f630a0efa37b28db
goditahi <131813354+goditahi@users.noreply.github.com>
2024-02-20 15:19:10 +0000

    "reduce method"

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,51 @@
-const findVowels=(str)=>{
+// const findVowels=(str)=>{
  
-    for(let i of str){
-        if (i==='a' || i=='e'  || i=='i'  || i=='o'  || i=='u'){
-            console.log(i)
-        }
-    }
-}
+//     for(let i of str){
+//         if (i==='a' || i=='e'  || i=='i'  || i=='o'  || i=='u'){
+//             console.log(i)
+//         }
+//     }
+// }
 
-let str="hello world";
-findVowels(str)
+// let str="hello world";
+// findVowels(str)
+
+// arr=[2,3,5,2,4]
+
+// arr.forEach(element => {
+//     console.log(element**2);
+// });
+
+// const square= arr.map(ele=> {return ele**2})
+// console.log(square)
+
+// const onlyEven=arr.filter(element=> {return element%2===0})
+// console.log(onlyEven)
+
+// const obj={
+//     name:"anshul",
+//     age:22,
+//     learning:'js'
+// }
+//const arr= obj.map((ele,ind)=>{return ele})
+//TypeError: obj.map is not a function
+// const arr= obj.map((ele,ind)=>{return ele})
+// console.log(arr)
+// let newArray=Object.keys(obj).map(ele=>{return ele})
+// console.log(newArray)
+
+// reduce function
+// arr=[1,2,3,4,4]
+
+// const sum=arr.reduce((acc,ind)=>{return acc+ind})
+// console.log(sum)
+
+const prices = {
+    a: 100,
+    b: 200,
+    c: 300
+  };
+
+  const totalPayment=Object.values(prices).reduce((acc,curr)=>{return acc+curr});
+  console.log(totalPayment)
+
