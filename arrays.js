31425ca79f88a84b9907717fe7a6e92f1c5d9811
Anshul Negi <anshuln168@gmail.com>
2024-02-20 15:00:30 +0530

    "add new files"

---
+++ arrays.js
@@ -0,0 +1,6 @@
+let a=[1,2,3,4,5,6];
+a.pop()    // remove from end
+a.shift()  // remove from front 
+a.unshift(45) // add in front
+a.push(343);  //add in last
+console.log(a)
---
+++ index.js
@@ -0,0 +1,39 @@
+// Guess Game answer
+// const answer=34;
+
+// let guess= Number(prompt("enter the value"));
+
+// while(guess!==answer){
+    
+//     guess=Number(prompt("enter the value again not correnct answer"));
+    
+// }
+
+// console.log("right answer");
+
+// let fullName=prompt("Enter your full Name");
+
+// let generatedAnswer="@"+fullName+fullName.length
+// let answer=""
+
+// for (let i of generatedAnswer){
+//     if(i!==" "){
+//         answer+=i
+//     }
+//     else{
+//         continue
+//     }
+// }
+
+// // for (let i in generatedAnswer){
+// //     if(generatedAnswer[i]!==" "){
+// //         answer+=generatedAnswer[i]
+// //     }
+// //     else{
+// //         continue
+// //     }
+// // }
+
+// console.log(answer)
+
+// let a=[1,2,3,4,5]
